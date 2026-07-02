cbe418ea7a110d71cee23305d76ea1a0265d9747
marie <mariecaron27@gmail.com>
2026-07-02 11:05:54 +0200

    The user can modifiy the content of a cell in the table view

--- ProductTableDelegate.cs
+++ ProductTableDelegate.cs
@@ -18,7 +18,7 @@ public class ProductTableDelegate : NSTableViewDelegate
     #endregion
 
     #region Override Methods
-    public override NSView GetViewForItem (NSTableView tableView, NSTableColumn tableColumn, nint row)
+    /*public override NSView GetViewForItem (NSTableView tableView, NSTableColumn tableColumn, nint row)
     {
       // This pattern allows you reuse existing views when they are no-longer in use.
       // If the returned view is null, you instance up a new view
@@ -44,6 +44,50 @@ public class ProductTableDelegate : NSTableViewDelegate
       }
 
       return view;
+    }*/
+    
+    public override NSView GetViewForItem (NSTableView tableView, NSTableColumn tableColumn, nint row) //Allows the user to edit the table view
+    {
+      // This pattern allows you reuse existing views when they are no-longer in use.
+      // If the returned view is null, you instance up a new view
+      // If a non-null view is returned, you modify it enough to reflect the new data
+      NSTextField view = (NSTextField)tableView.MakeView (tableColumn.Title, this);
+      if (view == null) {
+        view = new NSTextField ();
+        view.Identifier = tableColumn.Title;
+        view.BackgroundColor = NSColor.Clear;
+        view.Bordered = false;
+        view.Selectable = false;
+        view.Editable = true;
+
+        view.EditingEnded += (sender, e) => {
+
+          // Take action based on type
+          switch(view.Identifier) {
+            case "Product":
+              DataSource.Products [(int)view.Tag].Title = view.StringValue;
+              break;
+            case "Details":
+              DataSource.Products [(int)view.Tag].Description = view.StringValue;
+              break;
+          }
+        };
+      }
+
+      // Tag view
+      view.Tag = row;
+
+      // Setup view based on the column selected
+      switch (tableColumn.Title) {
+        case "Product":
+          view.StringValue = DataSource.Products [(int)row].Title;
+          break;
+        case "Details":
+          view.StringValue = DataSource.Products [(int)row].Description;
+          break;
+      }
+
+      return view;
     }
     #endregion
 
@@ -51,6 +95,29 @@ public class ProductTableDelegate : NSTableViewDelegate
     public override bool ShouldSelectRow (NSTableView tableView, nint row)
     {
       return true;
+    }
+    #endregion
+    
+    #region Reorder columns
+    public override bool ShouldReorder (NSTableView tableView, nint columnIndex, nint newColumnIndex)
+    {
+      return true;
+    }
+    #endregion
+
+    #region Caracter matches the first line of Product or Details
+    public override nint GetNextTypeSelectMatch (NSTableView tableView, nint startRow, nint endRow, string searchString)
+    {
+      nint row = 0;
+      foreach(Product product in DataSource.Products) {
+        if (product.Title.Contains(searchString)) return row;
+
+        // Increment row counter
+        ++row;
+      }
+
+      // If not found select the first row
+      return 0;
     }
     #endregion
 }
--- obj/Debug/net10.0-macos/osx-arm64/vues_table.AssemblyInfo.cs
+++ obj/Debug/net10.0-macos/osx-arm64/vues_table.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vues_table")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c66572d9c6c468b0f96f434e52672d63197bddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18d91930394027c6cb8760cd21e27b23bef1e6b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("vues_table")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vues_table")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
